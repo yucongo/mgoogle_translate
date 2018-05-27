a2a4faec928f86de8636c41e2fb7c4fa646ae9f8
yucongo <yucongo@gmail.com>
2018-05-27 12:04:49 +0800

    js signing converted to python code, faster

--- bd_fanyi.py
+++ bd_fanyi.py
@@ -1,21 +1,39 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 '''
 baidu fanyi, token diy
-OBRSTR and Cookie from: https://gitee.com/o0elias0o/eliaspython/tree/master
 
-# 需要安装的库
-# pip install requests
-# pip install PyExecJS
+sign_text_js.js
+sign.js from
+https://www.devtool.top/article/42/
+http://blog.csdn.net/m0_37953479/article/details/78929281
 
-PyExecJS 需 js 环境或解释器（如 nodejs 或 phantomjs.exe)
+sign_text_js.js ==js2py==>sign_text_js.py
+sign = sign_text_js.hash(text, GTK)
+
+https://www.jianshu.com/p/38a65d8d3e80
+	fetch cookie first
+
+resp = requests.get(, headers=HEADERS)
+TOKEN = re.findall(r"token: '(.*)'", resp.text)[0]
+
+URL0 = 'http://fanyi.baidu.com/'
+TOKEN = re.findall(r"token: '(.*)'", requests.get(URL0, headers=HEADERS).text)[0]
+
+
 '''
 
 from __future__ import print_function
 import logging
 # import json
 
+import re
 import requests
-import execjs
+# import execjs
+
+# from sign_text import sign_text
+from sign_text_js import sign_text_js
 
 # from utils import (obrstr, headers, gtk, token)
 # from utils import (obrstr, headers)
@@ -23,41 +41,31 @@ import execjs
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())
 
-OBRSTR = \
-'''function wods(r,gtk) {
-    function a(df, o) {
-        for (var t = 0; t < o.length - 2; t += 3) {
-            var a = o.charAt(t + 2);
-            a = a >= "a" ? a.charCodeAt(0) - 87 : Number(a), a = "+" === o.charAt(t + 1) ? df >>> a : df << a, df = "+" === o.charAt(t) ? df + a & 4294967295 : df ^ a
-        }
-        return df
-    }
-    var o = r.length;
-    o > 30 && (r = "" + r.substr(0, 10) + r.substr(Math.floor(o / 2) - 5, 10) + r.substr(-10, 10));
-    var t = gtk, e = t.split("."), h = Number(e[0]) || 0, i = Number(e[1]) || 0, d = [], f = 0;
-    for (var  g = 0; g < r.length; g++) {
-        var m = r.charCodeAt(g);
-        128 > m ? d[f++] = m : (2048 > m ? d[f++] = m >> 6 | 192 : (55296 === (64512 & m) && g + 1 < r.length && 56320 === (64512 & r.charCodeAt(g + 1)) ? (m = 65536 + ((1023 & m) << 10) + (1023 & r.charCodeAt(++g)), d[f++] = m >> 18 | 240, d[f++] = m >> 12 & 63 | 128) : d[f++] = m >> 12 | 224, d[f++] = m >> 6 & 63 | 128), d[f++] = 63 & m | 128)
-    }
-    var S = h, u = "+-a^+6", l = "+-3^+b+-f";
-    for (var s = 0; s < d.length; s++) S += d[s], S = a(S, u);
-    return S = a(S, l), S ^= i, 0 > S && (S = (2147483647 & S) + 2147483648), S %= 1e6, S.toString() + "." + (S ^ h) }'''  # NOQA
-
 HEADERS = {
     'Cookie':'BAIDUID=99C3CE30DA0FE8FD336F4428DA0DFDFC:FG=1; BIDUPSID=99C3CE30DA0FE8FD336F4428DA0DFDFC; PSTM=1515055428; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; locale=zh; PSINO=7; H_PS_PSSID=1443_25549_21127_22157; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1514859052,1515028770,1515029153,1515114213; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1515134327; from_lang_often=%5B%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D',  # NOQA
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'  # NOQA
 }
 
+URL0 = 'http://fanyi.baidu.com/'
+TOKEN = re.findall(r"token: '(.*)'", requests.get(URL0, headers=HEADERS).text)[0]
+
 GTK = '320305.131321201'
-TOKEN = '46e5646d48c43e676f955dc7fdece81f'
-PARSERCALL = execjs.compile(OBRSTR).call
+# TOKEN = '46e5646d48c43e676f955dc7fdece81f'
+# TOKEN = 'ecb9f034a0f6b4899b38204a821a6f14ecb9f034a0f6b4899b38204a821a6f14'
+
+# PARSERCALL = execjs.compile(OBRSTR).call
+
 # SIGN = lambda que: PARSERCALL("wods", que, GTK)
 URL = 'http://fanyi.baidu.com/v2transapi/'
 
-
+TMP = \
+"""
+# replace with sign_text.sign_text
+# time reduced from 1-2 sec/req to 0.6s/req
 def sign_text(text):
     '''sign text'''
     return PARSERCALL("wods", text, GTK)
+"""
 
 
 def bd_fanyi(text, from_lang='auto', to_lang='zh'):
@@ -73,19 +81,58 @@ def bd_fanyi(text, from_lang='auto', to_lang='zh'):
     py.test --doctest-modules bd_fanyi.py or
     nosetests --with-doctest bd_fanyi.p
 
+    text = 'test'
+    from_lang='auto'
+    to_lang='zh'
+
     >>> text = 'test'
     >>> bd_fanyi(text)[0].keys() == [{'result': [[0, '测试', ['0|4'], [], ['0|4'], ['0|6']]], 'prefixWrap': 0, 'dst': '测试', 'relation': [], 'src': 'test'}][0].keys()
     True
     >>> bd_fanyi(text)[0].get('dst')
     '测试'
+    >>> bd_fanyi('测试')[0].get('dst')
+    'test'
+    >>> bd_fanyi('测试', to_lang='en')[0].get('dst')
+    'test'
     '''
+    if isinstance(text, list):
+        text = ' '.join(text)
+
+    if from_lang is None:
+        from_lang = 'auto'
+    else:
+        from_lang = from_lang.strip()
+
+    if not from_lang:
+        from_lang = 'auto'
+    try:
+        from_lang = str(from_lang)
+    except Exception as exc:
+        del exc
+        from_lang = 'auto'
+
+    if to_lang is None:
+        to_lang = 'zh'
+    else:
+        to_lang = to_lang.strip()
+
+    if not to_lang:
+        to_lang = 'zh'
+    try:
+        to_lang = str(to_lang)
+    except Exception as exc:
+        del exc
+        to_lang = 'zh'
+
     text = text.strip()
 
     if not text.strip():
         return ''
 
     # sign = SIGN(text)
-    sign = sign_text(text)
+    # sign = sign_text(text)
+    sign = sign_text_js.hash(text, GTK)
+
     data = {
         'from': from_lang,
         'to': to_lang,
@@ -93,19 +140,36 @@ def bd_fanyi(text, from_lang='auto', to_lang='zh'):
         'transtype': 'realtime',
         'simple_means_flag': '3',
         'sign': sign,
-        'token': TOKEN
+        'token': TOKEN,
     }
+
+    # response = requests.post(URL, data=data, headers=HEADERS)
+    # response.json()
+
+	# response = requests.get('http://fanyi.baidu.com/', headers=HEADERS)
+	# response = REQ_SESS.get(URL)
+
+    # response = requests.get('https://fanyi.baidu.com/#en/zh/currency', verify=False)
 
     try:
         response = requests.post(URL, data=data, headers=HEADERS)
+
+        # response = REQ_SESS.post(URL, data=data, headers=HEADERS)
         # response.encoding = 'utf-8'
         response.raise_for_status()
     except Exception as exc:
         LOGGER.error(exc)
         return str(exc)
 
+
     try:
-        resu = response.json().get('trans_result').get('data')  # NOQA
+        j_data = response.json()
+        resu = j_data.get('trans_result')
+        if resu is None:
+            resu = j_data.get('error')
+            resu = 'errors occurred, code: %s' % resu
+        else:
+            resu = resu.get('data')
     except Exception as exc:
         LOGGER.error(exc)
         resu = str(exc)
